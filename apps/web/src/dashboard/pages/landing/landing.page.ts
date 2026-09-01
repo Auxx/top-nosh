525b3b13fe8a216f51e02b130fb7cf72164fbfbc
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2026-09-01 21:28:16 +0100

    Removed unused code

--- apps/web/src/dashboard/pages/landing/landing.page.ts
+++ apps/web/src/dashboard/pages/landing/landing.page.ts
@@ -1,6 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthenticationService } from '../../../auth/services/authentication/authentication.service';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-landing',
@@ -10,12 +8,4 @@ import { AuthenticationService } from '../../../auth/services/authentication/aut
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LandingPage {
-  private readonly router = inject(Router);
-
-  private readonly authenticationService = inject(AuthenticationService);
-
-  readonly logout = () => {
-    this.authenticationService.logout();
-    this.router.navigate([ '/' ]).then();
-  };
 }
