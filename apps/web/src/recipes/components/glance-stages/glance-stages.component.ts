18ab52b851abe968e9c5a2453b9c85e660bd5b64
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2026-09-03 15:44:17 +0100

    Finished At A Glance refactoring

--- apps/web/src/recipes/components/glance-stages/glance-stages.component.ts
+++ apps/web/src/recipes/components/glance-stages/glance-stages.component.ts
@@ -1,5 +1,11 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { MatCard, MatCardContent } from '@angular/material/card';
+import {
+  MatAccordion,
+  MatExpansionPanel,
+  MatExpansionPanelHeader,
+  MatExpansionPanelTitle
+} from '@angular/material/expansion';
 import { NoticeComponent, SectionHeaderComponent } from '@top-nosh/ui';
 import { RecipeDetails } from '../../models/recipe-details.types';
 
@@ -9,7 +15,11 @@ import { RecipeDetails } from '../../models/recipe-details.types';
     SectionHeaderComponent,
     MatCard,
     MatCardContent,
-    NoticeComponent
+    NoticeComponent,
+    MatAccordion,
+    MatExpansionPanel,
+    MatExpansionPanelHeader,
+    MatExpansionPanelTitle
   ],
   templateUrl: './glance-stages.component.html',
   styleUrl: './glance-stages.component.scss',
