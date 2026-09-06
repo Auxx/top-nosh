5c0028860cc60d4dcd560b3d8bc796cc6b6d665a
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2026-09-06 11:22:42 +0100

    The language depends on browser settings now

--- apps/web/src/app/app.config.ts
+++ apps/web/src/app/app.config.ts
@@ -1,7 +1,7 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter, TitleStrategy, withComponentInputBinding } from '@angular/router';
-import { provideTransloco } from '@jsverse/transloco';
+import { getBrowserLang, provideTransloco } from '@jsverse/transloco';
 import { authInterceptor } from '../auth/interceptors/auth/auth.interceptor';
 import { baseUrlInterceptor } from '../system/interceptors/base-url/base-url.interceptor';
 import { appRoutes } from './app.routes';
@@ -17,9 +17,10 @@ export const appConfig: ApplicationConfig = {
     provideTransloco({
       config: {
         availableLangs: [ 'en', 'ru' ],
-        defaultLang: 'en',
+        defaultLang: getBrowserLang(),
         reRenderOnLangChange: true,
-        prodMode: !isDevMode()
+        prodMode: !isDevMode(),
+        fallbackLang: 'en'
       },
       loader: TranslocoHttpLoader
     })
