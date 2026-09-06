da31aafadd1e838e825c0f8191ee1abd10a59065
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2026-09-06 10:54:38 +0100

    Updated base url interceptor to allow optional relative urls without modification

--- apps/web/src/app/transloco-loader.ts
+++ apps/web/src/app/transloco-loader.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpContext } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Translation, TranslocoLoader } from '@jsverse/transloco';
 import { HTTP_AUTH_ENABLED } from '../auth/interceptors/auth/auth.interceptor.types';
+import { HTTP_BASE_URL_ENABLED } from '../system/interceptors/base-url/base-url.interceptor.types';
 
 @Injectable({ providedIn: 'root' })
 export class TranslocoHttpLoader implements TranslocoLoader {
@@ -10,6 +11,10 @@ export class TranslocoHttpLoader implements TranslocoLoader {
   getTranslation = (lang: string) =>
     this.http.get<Translation>(
       `/assets/i18n/${lang}.json`,
-      { context: new HttpContext().set(HTTP_AUTH_ENABLED, false) }
+      {
+        context: new HttpContext()
+          .set(HTTP_AUTH_ENABLED, false)
+          .set(HTTP_BASE_URL_ENABLED, false)
+      }
     );
 }
--- apps/web/src/system/interceptors/base-url/base-url.interceptor.ts
+++ apps/web/src/system/interceptors/base-url/base-url.interceptor.ts
@@ -1,10 +1,17 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
+import { HTTP_BASE_URL_ENABLED } from './base-url.interceptor.types';
 
 const isAbsoluteUrl = (url: string): boolean => /^(?:[a-zA-Z][a-zA-Z0-9+.-]*:)?\/\//i.test(url);
 
 export const baseUrlInterceptor: HttpInterceptorFn = (req, next) => {
   if (isAbsoluteUrl(req.url)) {
+    return next(req);
+  }
+
+  const isBaseUrlEnabled = req.context.get(HTTP_BASE_URL_ENABLED);
+
+  if (!isBaseUrlEnabled) {
     return next(req);
   }
 
