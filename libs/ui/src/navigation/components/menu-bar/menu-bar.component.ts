c3508e37d4b27b0ab76434574be57e0ae9e2bd90
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2026-09-06 11:17:01 +0100

    Updated MenuBarComponent design and added translations

--- libs/ui/src/navigation/components/menu-bar/menu-bar.component.ts
+++ libs/ui/src/navigation/components/menu-bar/menu-bar.component.ts
@@ -3,7 +3,15 @@ import { MatButton, MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
 import { MatToolbar } from '@angular/material/toolbar';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { TranslocoDirective } from '@jsverse/transloco';
+
+const menuItems = (): { url: string; label: string; }[] => [
+  { url: '/dashboard', label: 'dashboard' },
+  { url: '/recipes', label: 'recipes' },
+  { url: '/shopping-lists', label: 'shoppingLists' },
+  { url: '/users', label: 'settings' }
+];
 
 @Component({
   selector: 'ui-menu-bar',
@@ -15,11 +23,14 @@ import { RouterLink } from '@angular/router';
     MatIcon,
     MatMenu,
     MatMenuTrigger,
-    MatMenuItem
+    MatMenuItem,
+    TranslocoDirective,
+    RouterLinkActive
   ],
   templateUrl: './menu-bar.component.html',
   styleUrl: './menu-bar.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MenuBarComponent {
+  readonly menuItems = menuItems();
 }
