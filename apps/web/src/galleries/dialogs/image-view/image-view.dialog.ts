21c56165cc088201114cf0cefd58a1622d429e4e
Aleksandr 'Aux' Ivanov <aux@hexmode.org>
2026-09-18 13:41:34 +0100

    Updated gallery design

--- apps/web/src/galleries/dialogs/image-view/image-view.dialog.ts
+++ apps/web/src/galleries/dialogs/image-view/image-view.dialog.ts
@@ -1,13 +1,11 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 
 export interface ImageViewDialogData {
   readonly imageUrl: string;
 }
-
-export type ImageViewData = ImageViewDialogData;
 
 @Component({
   selector: 'app-image-view',
@@ -22,8 +20,4 @@ export type ImageViewData = ImageViewDialogData;
 })
 export class ImageView {
   readonly data: ImageViewDialogData = inject(MAT_DIALOG_DATA);
-  readonly dialogRef = inject(MatDialogRef<ImageView>);
 }
-
-export const ImageViewDialog = ImageView;
-export type ImageViewDialog = ImageView;
